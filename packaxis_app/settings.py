7fb0d14fff6f239e74f9b3a09cd14117f94f50d7
printnkraft <printnkraft25@gmail.com>
2025-11-23 23:35:22 -0500

    Update email settings to use Hostinger SMTP server

--- packaxis_app/settings.py
+++ packaxis_app/settings.py
@@ -307,9 +307,10 @@ if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
-    EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-    EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+    EMAIL_HOST = config('EMAIL_HOST', default='smtp.hostinger.com')
+    EMAIL_PORT = config('EMAIL_PORT', default=465, cast=int)
+    EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=True, cast=bool)
+    EMAIL_USE_TLS = False  # SSL uses port 465, TLS uses port 587
     EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
     EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 
