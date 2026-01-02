cae3c7c5c776a4ee5ce91b58cf4c8160d987a164
printnkraft <printnkraft25@gmail.com>
2026-01-01 23:00:50 -0500

    Fix all migration dependencies: 0015 now depends on 0009 instead of deleted 0014

--- core/migrations/0015_sitesettings.py
+++ core/migrations/0015_sitesettings.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0014_product_canonical_url_product_feature_5_and_more'),
+        ('core', '0009_fake'),
     ]
 
     operations = [
